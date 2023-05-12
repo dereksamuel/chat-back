7429cfe716cae3e33cd4aa070099df5dcba3925a
Derek Paul <dereksamuelgr@gmail.com>
2023-05-12 17:45:13 -0500

    done security by course

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const routerApi = require("./router");
 const { errorHandler, logErrors, boomErrorHandler } = require("./middlewares/error.handler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const whiteList = ["http://localhost:8080"];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("[error]: Not allowed by CORS"));
+    }
+  }
+};
 
 app.use(express.json());
+app.use(cors(options));
 
 routerApi(app);
 
