bb37ecb762197a46e139780a66bf2c984c9a5f0b
Derek Paul <dereksamuelgr@gmail.com>
2023-05-17 16:12:05 -0500

    docker mysql config for my migrations ok

---
+++ db/config.js
@@ -0,0 +1,14 @@
+const { dbConfig } = require("../config/config");
+
+const URI = `mysql://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.name}`;
+
+module.exports = {
+  development: {
+    dialect: "mysql",
+    url: URI
+  },
+  production: {
+    dialect: "mysql",
+    url: URI
+  }
+};
